961461dbb10d9f1866379cf7a47e082c33ae25c4
dhanavanthesh <dhanavanthesh697@gmail.com>
2026-09-10 00:33:13 +0530

    Export semantic Python API

--- oc_sidememory/__init__.py
+++ oc_sidememory/__init__.py
@@ -2,7 +2,41 @@ import sys
 
 # kernels is not reexported as it should remain an optional dependency
 from . import _json_schema as json_schema
-from ._native import Guide, Index, Vocabulary
+from ._native import (
+    CompileError,
+    CompiledSchema,
+    Guide,
+    Index,
+    InternalInvariantError,
+    LifecycleError,
+    OcSidememoryError,
+    ResourceLimitError,
+    RollbackError,
+    SemanticViolation,
+    SidememoryGuide,
+    StructuralRejection,
+    Vocabulary,
+    compile_schema,
+)
+
+__all__ = [
+    "CompileError",
+    "CompiledSchema",
+    "Guide",
+    "Index",
+    "InternalInvariantError",
+    "LifecycleError",
+    "OcSidememoryError",
+    "ResourceLimitError",
+    "RollbackError",
+    "SemanticViolation",
+    "SidememoryGuide",
+    "StructuralRejection",
+    "Vocabulary",
+    "compile_schema",
+    "compat",
+    "json_schema",
+]
 
 # Register json_schema so direct submodule imports remain available.
 # import ..." works
